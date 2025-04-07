e658f5c8f6df280d97647d80a862b5aeac28b540
Thomaz Lucca <thomazlucca@gmail.com>
2025-04-07 09:26:47 -0300

    adicionando autores

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { autorSchema } from "./Autor.js";
 
 const livroSchema = new mongoose.Schema(
     {
@@ -6,7 +7,8 @@ const livroSchema = new mongoose.Schema(
         titulo: { type: String, required: true },
         editora: { type: String },
         preco: { type: Number },
-        paginas: { type: Number }
+        paginas: { type: Number },
+        autor: autorSchema
     },
     {
         versionKey: false
