69e6b3ac671ad23a87fd0c01ffd9e2afd99183de
Thomaz Lucca <thomazlucca@gmail.com>
2025-04-09 17:18:31 -0300

    validação de dados do mongoose

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -5,9 +5,19 @@ const livroSchema = new mongoose.Schema(
   {
     id: { type: mongoose.Schema.Types.ObjectId },
     titulo: { type: String, required: [true, "O título do livro é obrigatório."] },
-    editora: { type: String },
+    editora: { type: String, required: [true, "A Editora é obrigatória."], enum: {values: ["Casa do código", "Alura" ], message: "A Editora {VALUE} não é um valor permitido"}},
     preco: { type: Number },
-    paginas: { type: Number },
+    paginas: { 
+      type: Number,
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "O número de páginas deve estar entre 10 e 5000. Valor inserido: {VALUE}"
+      }
+      // min: [10, "O número de páginas deve estar entre 10 e 5000. Valor inserido: {VALUE}"],
+      // max: [5000, "O número de páginas deve estar entre 10 e 5000. Valor inserido: {VALUE}"] 
+    },
     autor: autorSchema
   },
   {
